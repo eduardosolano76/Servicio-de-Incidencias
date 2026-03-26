def267958cef80e93248ab69e7fd08b934314727
eduar <eduar@DESKTOP-MKGMTGO>
2026-03-25 19:23:56 -0600

    DTO

---
+++ ServicioIncidencias/src/main/java/com/example/demo/dto/HistorialEstadosDTO.java
@@ -0,0 +1,25 @@
+package com.example.demo.dto;
+
+import java.time.LocalDateTime;
+
+import com.example.demo.entity.Estado;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class HistorialEstadosDTO {
+	
+	private Long id;
+    private Long incidenciaId; 
+    private Estado estado;
+    private LocalDateTime fechaCambio;
+    private String comentarios;
+    private Long usuarioModificador;
+
+}
---
+++ ServicioIncidencias/src/main/java/com/example/demo/dto/IncidenciasDTO.java
@@ -0,0 +1,28 @@
+package com.example.demo.dto;
+
+import java.time.LocalDateTime;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class IncidenciasDTO {
+
+	private Long id;
+    private String titulo;
+    private String descripcion;
+    
+    // Reemplazamos el objeto completo por su ID para evitar problemas de recursividad en JSON
+    private Long tipoIncidenciaId; 
+    
+    private LocalDateTime fechaReporte;
+    private Long usuarioId;
+    private Long ubicacionId;
+    private Long departamentoId;
+    private Long personalId;
+}
