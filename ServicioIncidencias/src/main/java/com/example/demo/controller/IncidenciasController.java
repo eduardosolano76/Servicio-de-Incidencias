b62e42a2bb2a93b75f1b70d2f88e80af960a5b7f
eduar <eduar@DESKTOP-MKGMTGO>
2026-04-22 16:04:09 -0600

    Commit

--- ServicioIncidencias/src/main/java/com/example/demo/controller/IncidenciasController.java
+++ ServicioIncidencias/src/main/java/com/example/demo/controller/IncidenciasController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -22,6 +23,7 @@ import lombok.RequiredArgsConstructor;
 @RestController
 @RequestMapping("/api/incidencias")
 @RequiredArgsConstructor
+@CrossOrigin(origins = "*") // Permitir solicitudes desde cualquier origen (CORS)
 public class IncidenciasController {
 	
 	private final IncidenciasService incidenciasService;
@@ -34,7 +36,8 @@ public class IncidenciasController {
 	}
 
     // 2. Obtener todas las incidencias
-    @GetMapping
+    //@PreAuthorize("hasRole('ADMIN') or hasRole('SISTEMA')")
+	@GetMapping
     public ResponseEntity<List<IncidenciasDTO>> obtenerTodas() {
         return ResponseEntity.ok(incidenciasService.obtenerTodas());
     }
